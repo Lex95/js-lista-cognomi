ba62305139044664fe166b0dcb612c3f37e3cfe3
Lex95 <andrea.lega1995@gmail.com>
2021-04-13 17:35:51 +0200

    tentata aggiunta di un sort personalizzato (lasciato tra i commenti)

--- myscript.js
+++ myscript.js
@@ -14,3 +14,44 @@ for (var i = 0; i < newLista.length; i++) {
 }
 
 document.getElementById("myNum").innerHTML += newLista.indexOf(newCognome) + 1;
+
+// bonus my sort (lascio commentato perchè non funziona)
+
+// var newList = [];
+// var sorter = [];
+// var h = 0;
+// var max;
+
+// do {
+//     for (var j = 0; j < listaCognomi; j++) {
+//         for (var k = 0; k < listaCognomi; k++) {
+//             if (listaCognomi[j][h] > listaCognomi[k][h]) {
+//                 sorter[k] = true;
+//             } else if (listaCognomi[j][h] < listaCognomi[k][h]) {
+//                 sorter[k] = false;
+//             } else if ((listaCognomi[j][h] == listaCognomi[k][h]) && (h < listaCognomi[j].length) && (h < listaCognomi[k].length)) {
+//                 h++;
+//                 k--;
+//             } else {
+//                 sorter[k] = true;
+//                 h = 0;
+//             }
+//         }
+//         k = 0;
+//         do {
+//             max = sorter[k];
+//             k++;
+//         } while ((max == true) && (k < listaCognomi));
+//         if (max == true || listaCognomi.length == 1) {
+//             newList.push(listaCognomi[j]);
+//             listaCognomi.splice(j, 1);
+//             break;
+//         }
+//     }
+// } while (listaCognomi.length > 0)
+
+// for (var i = 0; i < newList.length; i++) {
+//     document.getElementById("myId").innerHTML += "<li>" + newList[i] + "</li>";
+// }
+
+// document.getElementById("myNum").innerHTML += newList.indexOf(newCognome) + 1;
