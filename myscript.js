5aca1f420589d42a21c46ad470cfcf0d4cd521fe
Lex95 <andrea.lega1995@gmail.com>
2021-04-13 15:54:52 +0200

    aggiunta implementazione per la maiuscola

--- myscript.js
+++ myscript.js
@@ -2,6 +2,9 @@ var listaCognomi = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
 
 var newCognome = prompt("Inserisci il tuo cognome:");
 
+// mette la maiuscola nel caso sia minuscolo
+newCognome = newCognome[0].toUpperCase() + newCognome.slice(1);
+
 listaCognomi.push(newCognome);
 
 var newLista = listaCognomi.sort();
